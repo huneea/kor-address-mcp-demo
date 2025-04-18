f756bb8790338f77e5854ebd6c4348b68eb50b21
정훈희/AI서비스개발팀/NE <hhjung@nhn.com>
2025-04-18 13:55:37 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/example/address/KorAddressMcpDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.address;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KorAddressMcpDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KorAddressMcpDemoApplication.class, args);
+    }
+
+}
